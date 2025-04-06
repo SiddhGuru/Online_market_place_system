f05a1a725f6c15b3ff6613da28f5ca9b77eee232
Sidharth Powar <powarsidharth903@gmail.com>
2025-04-07 03:06:27 +0530

    first-commit

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ app.use('/api/products', productRoutes);
 app.use('/api/auth', authRoutes);
 
 // Server
-const PORT = 5000;
+const PORT = process.env.PORT|| 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
